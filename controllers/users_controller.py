e20848e7c603dbd948a0a0acd2db67c0144c6db4
Hayden Burker <hayden.burker@gmail.com>
2024-12-24 10:16:58 -0700

    adds remaining crud routes and controllers

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -21,4 +21,104 @@ def add_user():
         "last_name": last_name,
         "email": email,
         "active": active
-    }})
+    }}), 201
+
+def get_all_users():
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    get_all_query = 'SELECT user_id, first_name, last_name, email, active FROM "Users"'
+    cursor.execute(get_all_query)
+
+    users = cursor.fetchall()
+
+    users = [{"user_id": user_id, "first_name": first_name, "last_name": last_name, "email": email, "active": active} for [user_id, first_name, last_name, email, active] in users]
+
+    return jsonify({"message": "users found", "results": users}), 200
+
+def get_user_by_id(user_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    get_by_id_query = """SELECT user_id, first_name, last_name, email, active FROM "Users"
+    WHERE user_id = %s"""
+    cursor.execute(get_by_id_query, (user_id,))
+    user = cursor.fetchone()
+    if not user:
+        return jsonify({"message": "user not found"}), 404
+
+    [user_id, first_name, last_name, email, active] = user
+    user = {"user_id": user_id, "first_name": first_name, "last_name": last_name, "email": email, "active": active}
+    return jsonify({"message": "user found", "results": user}), 200
+
+def update_user(user_id):
+    post_data = request.json
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    get_by_id_query = """SELECT * FROM "Users"
+    WHERE user_id = %s"""
+    cursor.execute(get_by_id_query, (user_id,))
+    user = cursor.fetchone()
+    if not user:
+        return jsonify({"message": "user not found"}), 404
+
+    [user_id, first_name, last_name, email, password, active] = user
+
+    update_query = """UPDATE "Users"
+    SET first_name = %s,
+    last_name = %s,
+    email = %s,
+    password = %s
+    WHERE user_id = %s RETURNING *"""
+    cursor.execute(update_query, (post_data.get("first_name") or first_name, post_data.get("last_name") or last_name, post_data.get("email") or email, post_data.get("password") or password, user_id))
+    user = cursor.fetchone()
+    connection.commit()
+
+    [user_id, first_name, last_name, email, password, active] = user
+    user = {"user_id": user_id, "first_name": first_name, "last_name": last_name, "email": email, "active": active}
+    return jsonify({"message": "user updated", "results": user}), 200
+
+def user_activity(user_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    get_by_id_query = """SELECT active FROM "Users"
+    WHERE user_id = %s"""
+    cursor.execute(get_by_id_query, (user_id,))
+    user = cursor.fetchone()
+    if not user:
+        return jsonify({"message": "user not found"}), 404
+
+    [active] = user
+
+    activity_query = """UPDATE "Users"
+    SET active = %s
+    WHERE user_id = %s RETURNING *"""
+    cursor.execute(activity_query, (not active, user_id))
+    user = cursor.fetchone()
+    connection.commit()
+
+    [user_id, first_name, last_name, email, password, active] = user
+    user = {"user_id": user_id, "first_name": first_name, "last_name": last_name, "email": email, "active": active}
+    return jsonify({"message": f"user {'activated' if active else 'deactivated'}", "results": user}), 200
+
+def delete_user(user_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    get_by_id_query = """SELECT user_id FROM "Users"
+    WHERE user_id = %s"""
+    cursor.execute(get_by_id_query, (user_id,))
+    user = cursor.fetchone()
+    if not user:
+        return jsonify({"message": "user not found"}), 404
+
+    [user_id] = user
+
+    delete_query = """DELETE FROM "Users"
+    WHERE user_id = %s"""
+    cursor.execute(delete_query, (user_id,))
+    connection.commit()
+
+    return jsonify({"message": "user deleted"}), 200
--- routes/users_routes.py
+++ routes/users_routes.py
@@ -7,3 +7,23 @@ users = Blueprint("users", __name__)
 @users.route("/user", methods=["POST"])
 def add_user():
     return users_controller.add_user()
+
+@users.route("/users", methods=["GET"])
+def get_all_users():
+    return users_controller.get_all_users()
+
+@users.route("/user/<user_id>", methods=["GET"])
+def get_user_by_id(user_id):
+    return users_controller.get_user_by_id(user_id)
+
+@users.route("/user/<user_id>", methods=["PUT"])
+def update_user(user_id):
+    return users_controller.update_user(user_id)
+
+@users.route("/user/<user_id>", methods=["PATCH"])
+def user_activity(user_id):
+    return users_controller.user_activity(user_id)
+
+@users.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    return users_controller.delete_user(user_id)
