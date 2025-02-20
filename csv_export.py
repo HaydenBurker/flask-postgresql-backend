7bfa735dac7efac0da4b4896a9185f9dfc6d243a
Hayden Burker <hayden.burker@gmail.com>
2025-02-19 23:06:22 -0700

    adds user csv export

--- csv_export.py
+++ csv_export.py
@@ -1,6 +1,16 @@
 import sys
+import csv
+
+from db import cursor
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        with open(f"csv/export/{sys.argv[1]}", "x") as export_file:
-            pass
+        with open(f"csv/export/{sys.argv[1]}", "w") as export_file:
+            user_fields = ["user_id","first_name","last_name","email","password","active","created_at","updated_at"]
+            users_query = f'SELECT {",".join(user_fields)} FROM "Users"'
+            cursor.execute(users_query)
+            users = cursor.fetchall()
+
+            csv_writer = csv.writer(export_file)
+            csv_writer.writerow(user_fields)
+            csv_writer.writerows(users)
