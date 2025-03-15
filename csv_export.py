2dec49d28c2762405343a6cbacfe97bb3de11bf0
Hayden Burker <hayden.burker@gmail.com>
2025-03-15 09:34:08 -0600

    adds another parameter for picking the table to export

--- csv_export.py
+++ csv_export.py
@@ -3,14 +3,25 @@ import csv
 
 from db import cursor
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        with open(f"csv/export/{sys.argv[1]}", "w") as export_file:
-            user_fields = ["user_id","first_name","last_name","email","password","active","created_at","updated_at"]
-            users_query = f'SELECT {",".join(user_fields)} FROM "Users"'
-            cursor.execute(users_query)
-            users = cursor.fetchall()
+table_fields = {
+    "Users": ["user_id", "first_name", "last_name", "email", "password", "active", "created_at", "updated_at"]
+}
 
-            csv_writer = csv.writer(export_file)
-            csv_writer.writerow(user_fields)
-            csv_writer.writerows(users)
+def export_table(table_name, fields, file_name):
+    with open(f"csv/export/{file_name}", "w") as export_file:
+        query = f'SELECT {",".join(fields)} FROM "{table_name}"'
+        cursor.execute(query)
+        records = cursor.fetchall()
+
+        csv_writer = csv.writer(export_file)
+        csv_writer.writerow(fields)
+        csv_writer.writerows(records)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        table_name = sys.argv[1]
+        file_name = sys.argv[2]
+
+        if table_name in table_fields:
+            fields = table_fields[table_name]
+            export_table(table_name, fields, file_name)
