885c6f96bae68af3ad23c65bbd69a5d0b7a8e991
Hayden Burker <hayden.burker@gmail.com>
2025-03-22 07:59:17 -0600

    fixes getting order items when no orders or products exist

--- controllers/order_items_controller.py
+++ controllers/order_items_controller.py
@@ -11,23 +11,24 @@ def create_order_item_object(order_item_data, many=False):
     order_ids = tuple(order["order_id"] for order in order_items)
     product_ids = tuple(order["product_id"] for order in order_items)
 
-    orders_query = """SELECT * FROM "Orders"
-    WHERE order_id in %s"""
-    cursor.execute(orders_query, (order_ids,))
-    orders = cursor.fetchall()
-    order_item_order_mapping = create_record_mapping(orders, base_order_object, key="order_id")
+    if order_items:
+        orders_query = """SELECT * FROM "Orders"
+        WHERE order_id in %s"""
+        cursor.execute(orders_query, (order_ids,))
+        orders = cursor.fetchall()
+        order_item_order_mapping = create_record_mapping(orders, base_order_object, key="order_id")
 
-    products_query = """SELECT * FROM "Products"
-    WHERE product_id in %s"""
-    cursor.execute(products_query, (product_ids,))
-    products = cursor.fetchall()
-    order_item_product_mapping = create_record_mapping(products, base_product_object, key="product_id")
+        products_query = """SELECT * FROM "Products"
+        WHERE product_id in %s"""
+        cursor.execute(products_query, (product_ids,))
+        products = cursor.fetchall()
+        order_item_product_mapping = create_record_mapping(products, base_product_object, key="product_id")
 
-    for i, order_item in enumerate(order_items):
-        order_items[i]["orders"] = order_item_order_mapping.get(order_item["order_id"])
-        order_items[i]["products"] = order_item_product_mapping.get(order_item["product_id"])
-        del order_items[i]["order_id"]
-        del order_items[i]["product_id"]
+        for i, order_item in enumerate(order_items):
+            order_items[i]["orders"] = order_item_order_mapping.get(order_item["order_id"])
+            order_items[i]["products"] = order_item_product_mapping.get(order_item["product_id"])
+            del order_items[i]["order_id"]
+            del order_items[i]["product_id"]
 
     return order_items if many else order_items[0]
 
