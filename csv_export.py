96d5643fbd1844872f3d78d19818097465ecb20a
Hayden Burker <hayden.burker@gmail.com>
2025-03-26 23:52:42 -0600

    adds export payments option

--- csv_export.py
+++ csv_export.py
@@ -5,7 +5,8 @@ from db import cursor
 
 table_fields = {
     "Users": ["user_id", "first_name", "last_name", "email", "password", "active", "created_at", "updated_at"],
-    "Orders": ["order_id", "customer_id", "order_date", "shipping_date", "status", "total_amount", "active", "created_at", "updated_at"]
+    "Orders": ["order_id", "customer_id", "order_date", "shipping_date", "status", "total_amount", "active", "created_at", "updated_at"],
+    "Payments": ["payment_id", "order_id", "payment_method", "payment_date", "payment_status", "payment_amount"]
 }
 
 def export_table(table_name, fields, file_name):
