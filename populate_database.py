db96d93d6aef0bb5265aae6596a1549fb3367925
Hayden Burker <hayden.burker@gmail.com>
2025-04-11 23:58:32 -0600

    adds multiple users

--- populate_database.py
+++ populate_database.py
@@ -11,9 +11,17 @@ def random_letters():
     return "".join(random.choice((string.ascii_lowercase)) for _ in range(random_length))
 
 def populate_database():
-    query = """INSERT INTO "Users" (user_id, first_name, last_name, email, password, active, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+    user_count = 5
+    query = """INSERT INTO "Users" (user_id, first_name, last_name, email, password, active, created_at, updated_at) VALUES """
+    values = "(%s, %s, %s, %s, %s, %s, %s, %s)"
+    query += ",".join(values for _ in range(user_count))
     current_date = datetime_now()
-    cursor.execute(query, (str(uuid.uuid4()), random_letters(), random_letters(), random_letters(), random_letters(), True, current_date, current_date))
+
+    records = []
+    for _ in range(user_count):
+        records += [str(uuid.uuid4()), random_letters(), random_letters(), random_letters(), random_letters(), True, current_date, current_date]
+
+    cursor.execute(query, records)
     connection.commit()
 
 if __name__ == "__main__":
