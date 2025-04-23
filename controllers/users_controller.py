b91a64cc95c4b00cd0fbdaf88f05e540c5d4a01b
Hayden Burker <hayden.burker@gmail.com>
2025-04-23 00:08:08 -0600

    adds some nested order fields in users, removes redundant ids

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -4,6 +4,8 @@ from models.users import base_user_object
 from models.products import base_product_object
 from models.categories import base_category_object
 from models.orders import base_order_object
+from models.shippings import base_shipping_object
+from models.payments import base_payment_object
 from util.records import create_record_mapping
 
 
@@ -36,6 +38,23 @@ def create_user_object(user_data, many=False):
         cursor.execute(orders_query, (user_ids,))
         orders = cursor.fetchall()
     user_order_mapping = create_record_mapping(orders, base_order_object, key="customer_id", many=True)
+    order_ids = tuple(set(order[0] for order in orders))
+
+    shippings = []
+    if order_ids:
+        shippings_query = """SELECT shipping_id, order_id, shipping_address, shipping_label, shipping_cost, tracking_number, shipping_status, shipped_date FROM "Shippings"
+        WHERE order_id IN %s"""
+        cursor.execute(shippings_query, (order_ids,))
+        shippings = cursor.fetchall()
+    order_shipping_mapping = create_record_mapping(shippings, base_shipping_object, key="order_id")
+
+    payments = []
+    if order_ids:
+        payments_query = """SELECT payment_id, order_id, payment_method, payment_date, payment_status, payment_amount FROM "Payments"
+        WHERE order_id IN %s"""
+        cursor.execute(payments_query, (order_ids,))
+        payments = cursor.fetchall()
+    order_payment_mapping = create_record_mapping(payments, base_payment_object, key="order_id", many=True)
 
     for i, user in enumerate(users):
         user_id = user["user_id"]
@@ -45,6 +64,19 @@ def create_user_object(user_data, many=False):
         for product in users[i]["products"]:
             product_id = product["product_id"]
             product["categories"] = product_category_mapping.get(product_id, [])
+
+            del product["created_by_id"]
+
+        for order in users[i]["orders"]:
+            order_id = order["order_id"]
+            order["shipping"] = order_shipping_mapping.get(order_id, None)
+            order["payments"] = order_payment_mapping.get(order_id, [])
+
+            del order["customer_id"]
+            if order["shipping"]:
+                del order["shipping"]["order_id"]
+            for payment in order["payments"]:
+                del payment["order_id"]
 
     return users if many else users[0]
 
