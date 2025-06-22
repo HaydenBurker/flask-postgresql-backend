58b6e272459f3ebe5cc06a3dc68651dacfacf723
Hayden Burker <hayden.burker@gmail.com>
2025-06-21 22:21:30 -0600

    updates reviews record dump

--- controllers/reviews_controller.py
+++ controllers/reviews_controller.py
@@ -2,8 +2,8 @@ from db import cursor
 
 from .base_controller import BaseController
 from models.reviews import Review
-from models.users import base_user_object
-from models.products import base_product_object
+from models.users import User
+from models.products import Product
 from util.records import create_record_mapping
 
 def create_review_object(review_data, many=False):
@@ -13,19 +13,19 @@ def create_review_object(review_data, many=False):
 
     users = []
     if customer_ids:
-        users_query = """SELECT user_id, first_name, last_name, email, active, created_at, updated_at FROM "Users"
+        users_query = """SELECT * FROM "Users"
         WHERE user_id IN %s"""
         cursor.execute(users_query, (customer_ids,))
-        users = cursor.fetchall()
-    review_user_mapping = create_record_mapping(users, base_user_object, key="user_id")
+        users = User.load_many(cursor.fetchall())
+    review_user_mapping = create_record_mapping(users)
 
     products = []
     if product_ids:
         products_query = """SELECT * FROM "Products"
         WHERE product_id IN %s"""
         cursor.execute(products_query, (product_ids,))
-        products = cursor.fetchall()
-    review_product_mapping = create_record_mapping(products, base_product_object, key="product_id")
+        products = Product.load_many(cursor.fetchall())
+    review_product_mapping = create_record_mapping(products)
 
     for i, review in enumerate(reviews):
         reviews[i]["customer"] = review_user_mapping.get(review["customer_id"])
