87a826b7af59ea8ba62b6f483def674ffefa98c1
Hayden Burker <hayden.burker@gmail.com>
2025-07-05 23:40:20 -0600

    converts populate database script to using model classes

--- populate_database.py
+++ populate_database.py
@@ -3,23 +3,20 @@ import random
 import uuid
 
 from db import connection, cursor
+from models.users import User
+from models.orders import Order
+from models.products import Product
+from models.order_items import OrderItem
+from models.categories import Category
+from models.products_categories_xref import ProductCategory
+from models.discounts import Discount
+from models.orders_discounts_xref import OrderDiscount
+from models.shippings import Shipping
+from models.payments import Payment
+from models.reviews import Review
+from models.suppliers import Supplier
+from models.product_suppliers import ProductSupplier
 from util.datetime import datetime_now
-
-table_fields = {
-    "Users": ["user_id", "first_name", "last_name", "email", "password", "active", "created_at", "updated_at"],
-    "Orders": ["order_id", "customer_id", "order_date", "shipping_date", "status", "total_amount", "active", "created_at", "updated_at"],
-    "Shippings": ["shipping_id", "order_id", "shipping_address", "shipping_label", "shipping_cost", "tracking_number", "shipping_status", "shipped_date"],
-    "Payments": ["payment_id", "order_id", "payment_method", "payment_date", "payment_status", "payment_amount"],
-    "OrdersDiscountsXref": ["order_id", "discount_id"],
-    "Discounts": ["discount_id", "discount_code", "discount_type", "discount_value", "start_date", "end_date", "min_order_amount"],
-    "OrderItems": ["order_item_id", "order_id", "product_id", "quantity", "unit_price"],
-    "Products": ["product_id", "name", "description", "price", "stock_quantity", "created_by_id", "created_at", "updated_at"],
-    "ProductsCategoriesXref": ["product_id", "category_id"],
-    "Categories": ["category_id", "name", "description"],
-    "Reviews": ["review_id", "customer_id", "product_id", "rating", "comment", "created_at"],
-    "ProductSuppliers": ["product_id", "supplier_id", "supply_price", "supply_date"],
-    "Suppliers": ["supplier_id", "company_name", "contact_name", "email", "phone_number", "address", "active"]
-}
 
 class Total:
     users = 5
@@ -40,11 +37,11 @@ def random_letters():
     random_length = random.randint(8, 15)
     return "".join(random.choice((string.ascii_lowercase)) for _ in range(random_length))
 
-def create_records(records, table_name):
-    fields = table_fields[table_name]
+def create_records(records, model):
+    fields = model().dump_update().keys()
     record_field_count = len(fields)
     user_count = len(records) // record_field_count
-    query = f"""INSERT INTO "{table_name}" ({",".join(fields)}) VALUES """
+    query = f"""INSERT INTO "{model.tablename}" ({",".join(fields)}) VALUES """
     values = f"({','.join('%s' for _ in range(record_field_count))})"
     query += ",".join(values for _ in range(user_count))
     cursor.execute(query, records)
@@ -58,7 +55,7 @@ def populate_database():
         id = str(uuid.uuid4())
         user_ids.append(id)
         records += [id, random_letters(), random_letters(), random_letters() + "@" + random_letters() + "." + random_letters(), random_letters(), True, current_date, current_date]
-    create_records(records, "Users")
+    create_records(records, User)
 
     records = []
     order_ids = []
@@ -67,7 +64,7 @@ def populate_database():
         user_id = random.choice(user_ids)
         order_ids.append(id)
         records += [id, user_id, current_date, current_date, random_letters(), random.randint(1, 10), True, current_date, current_date]
-    create_records(records, "Orders")
+    create_records(records, Order)
 
     records = []
     product_ids = []
@@ -76,7 +73,7 @@ def populate_database():
         user_id = random.choice(user_ids)
         product_ids.append(id)
         records += [id, random_letters(), random_letters(), random.randint(1, 10), random.randint(0, 10), user_id, current_date, current_date]
-    create_records(records, "Products")
+    create_records(records, Product)
 
     records = []
     for _ in range(Total.order_items):
@@ -84,7 +81,7 @@ def populate_database():
         product_id = random.choice(product_ids)
         order_item_id = str(uuid.uuid4())
         records += [order_item_id, order_id, product_id, random.randint(1, 10), random.randint(1, 10)]
-    create_records(records, "OrderItems")
+    create_records(records, OrderItem)
 
     records = []
     category_ids = []
@@ -92,7 +89,7 @@ def populate_database():
         category_id = str(uuid.uuid4())
         category_ids.append(category_id)
         records += [category_id, random_letters(), random_letters()]
-    create_records(records, "Categories")
+    create_records(records, Category)
 
     records = []
     product_category_ids = set()
@@ -106,7 +103,7 @@ def populate_database():
 
         product_category_ids.add(id)
         records += [*id]
-    create_records(records, "ProductsCategoriesXref")
+    create_records(records, ProductCategory)
 
     records = []
     discount_ids = []
@@ -114,7 +111,7 @@ def populate_database():
         discount_id = str(uuid.uuid4())
         discount_ids.append(discount_id)
         records += [discount_id, random_letters(), random_letters(), random.randint(1, 10), current_date, current_date, random.randint(1, 10)]
-    create_records(records, "Discounts")
+    create_records(records, Discount)
 
     records = []
     order_disount_ids = set()
@@ -128,20 +125,20 @@ def populate_database():
 
         order_disount_ids.add(id)
         records += [*id]
-    create_records(records, "OrdersDiscountsXref")
+    create_records(records, OrderDiscount)
 
     records = []
     for order_id in random.sample(order_ids, Total.shippings):
         shipping_id = str(uuid.uuid4())
         records += [shipping_id, order_id, random_letters(), random_letters(), random.randint(1, 10), random_letters(), random_letters(), current_date]
-    create_records(records, "Shippings")
+    create_records(records, Shipping)
 
     records = []
     for _ in range(Total.payments):
         payment_id = str(uuid.uuid4())
         order_id = random.choice(order_ids)
         records += [payment_id, order_id, random_letters(), current_date, random_letters(), random.randint(1, 10)]
-    create_records(records, "Payments")
+    create_records(records, Payment)
 
     records = []
     for _ in range(Total.reviews):
@@ -149,7 +146,7 @@ def populate_database():
         customer_id = random.choice(user_ids)
         product_id = random.choice(product_ids)
         records += [review_id, customer_id, product_id, random.randint(1, 5), random_letters(), current_date]
-    create_records(records, "Reviews")
+    create_records(records, Review)
 
     records = []
     supplier_ids = []
@@ -157,7 +154,7 @@ def populate_database():
         supplier_id = str(uuid.uuid4())
         supplier_ids.append(supplier_id)
         records += [supplier_id, random_letters(), random_letters(), random_letters(), random_letters(), random_letters(), True]
-    create_records(records, "Suppliers")
+    create_records(records, Supplier)
 
     records = []
     product_supplier_ids = set()
@@ -171,7 +168,7 @@ def populate_database():
 
         product_supplier_ids.add(id)
         records += [*id, random.randint(1, 10), current_date]
-    create_records(records, "ProductSuppliers")
+    create_records(records, ProductSupplier)
     connection.commit()
 
 if __name__ == "__main__":
