57b76b583e6dc622cfa9f59d20871506cefc5f50
Hayden Burker <hayden.burker@gmail.com>
2025-08-20 23:18:56 -0600

    removes old record mapping function

--- util/records.py
+++ util/records.py
@@ -5,28 +5,6 @@ def serialize(obj):
         return obj and obj.isoformat()
     return obj
 
-def create_record_mapping(records, base_record_object, key=None, many=False):
-    record_mapping = {}
-
-    for record in records:
-        if not key:
-            record_key = record[-1]
-
-        record = base_record_object(record)
-
-        if key:
-            record_key = record[key]
-
-        if many:
-            if not record_mapping.get(record_key):
-                record_mapping[record_key] = [record]
-            else:
-                record_mapping[record_key].append(record)
-        else:
-            record_mapping[record_key] = record
-
-    return record_mapping
-
 def create_record_mapping(records, key=None, many=False):
     record_mapping = {}
 
