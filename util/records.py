bbe8f62d9fa7f1af7fb180645fa589a8ea9c88e2
Hayden Burker <hayden.burker@gmail.com>
2025-09-11 23:25:10 -0600

    simplifies create record mapping

--- util/records.py
+++ util/records.py
@@ -9,16 +9,10 @@ def create_record_mapping(records, key=None, many=False):
     record_mapping = {}
 
     for record in records:
-        if key:
-            record_key = getattr(record, key)
-        else:
-            record_key = getattr(record, record.primary_key)
+        record_key = getattr(record, key or record.primary_key)
 
         if many:
-            if not record_mapping.get(record_key):
-                record_mapping[record_key] = [record.dump()]
-            else:
-                record_mapping[record_key].append(record.dump())
+            record_mapping.setdefault(record_key, []).append(record.dump())
         else:
             record_mapping[record_key] = record.dump()
 
